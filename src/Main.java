5336ce81389f182ad25d394beaa544a07702b7aa
minaseyf <mina.seyff@gmail.com>
2025-05-07 09:23:43 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
 
         System.out.println("heelo how are you kdanfkwenafkakflkal");
 
+                System.out.println("heelo how are you kdanfkwenafkakflkal");
+                System.out.println("heelo how are you kdanfkwenafkakflkal");
     }
 }
